501d56a1a38f0a1621c0f2ac63063fd0cb6dd317
Gileno Duarte <gileno.cr.duarte@gmail.com>
2023-02-01 17:47:52 -0300

    adicionado elemementos de estoque

--- api/estoque/models.py
+++ api/estoque/models.py
@@ -1,8 +1,22 @@
 from django.db import models
-
+import uuid
 # reate your models here.
 
-class Itens(models.Model):
+
+
+class Item(models.Model):
+    uuid=models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     title=models.CharFild(max_legth=255)
-    value=models.FloatField()
+    price=models.FloatField()
+    description=models.TextField(max_length=500)
+    created_on=models.DateTimeField(auto_now_add=True)
+    estoque=models.ForeignKey()
+
+class estoque(models.Model):
+    uuid=models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    
+
+
+
+
 
