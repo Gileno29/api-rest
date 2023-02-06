baae01cbbcd8d7f5cd3683cc69c1dec22940c556
Gileno Duarte <gileno.cr.duarte@gmail.com>
2023-02-06 11:11:00 -0300

    Adicionado template para index

--- api/estoque/models.py
+++ api/estoque/models.py
@@ -1,20 +1,28 @@
 from django.db import models
+from  datetime import datetime
+from django.utils import timezone
 import uuid
 # reate your models here.
 
 
 
+
 class Item(models.Model):
     uuid=models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    title=models.CharFild(max_legth=255)
+    title=models.CharField(max_length=500)
     price=models.FloatField()
     description=models.TextField(max_length=500)
     created_on=models.DateTimeField(auto_now_add=True)
-    estoque=models.ForeignKey()
+    typeItem= models.CharField(max_length=255)
+    estoque=models.ForeignKey("Estoque", on_delete=models.DO_NOTHING)
 
-class estoque(models.Model):
+class Estoque(models.Model):
     uuid=models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    
+    name=models.CharField(max_length=50, null=False, default='Meu estoque')  
+    create_on=models.DateTimeField(default=timezone.now)
+
+
+
 
 
 
