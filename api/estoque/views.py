d532076a976b62cbe5c106d2c0b62bac709ff2c1
Gileno Duarte <gileno.cr.duarte@gmail.com>
2023-02-06 11:36:17 -0300

    iniciado crud

--- api/estoque/views.py
+++ api/estoque/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'front_end_crud/index.html')
+
+
+def addItem(request):
+    pass
+
+def viewItens(request):
+    pass
