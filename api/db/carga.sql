8bad14cda0416cc544e6448a6e6b17efa33dd1ba
Gileno Duarte <gileno.cr.duarte@gmail.com>
2023-02-21 12:23:34 -0300

    adicionado busca por itens

--- api/db/carga.sql
+++ api/db/carga.sql
@@ -1,1 +1,27 @@
-create database estoque;
+
+CREATE database IF NOT EXISTS estoque;
+
+INSERT INTO public.estoque_item
+(uuid, title, price, description, created_on, type_item, estoque_id)
+VALUES('56a8266e-b1ee-11ed-afa1-0242ac120002', 'caneta', 0, 'canetas', '', 'Nao Perecivel', NULL);
+
+INSERT INTO public.estoque_item
+(uuid, title, price, description, created_on, type_item, estoque_id)
+VALUES('99113f80-b1f4-11ed-afa1-0242ac120002', '', 0, '', '', '', 0);
+
+INSERT INTO public.estoque_item
+(uuid, title, price, description, created_on, type_item, estoque_id)
+VALUES(?, '', 0, '', '', '', 0);
+
+INSERT INTO public.estoque_item
+(uuid, title, price, description, created_on, type_item, estoque_id)
+VALUES(?, '', 0, '', '', '', 0);
+
+INSERT INTO public.estoque_item
+(uuid, title, price, description, created_on, type_item, estoque_id)
+VALUES(?, '', 0, '', '', '', 0);
+
+INSERT INTO public.estoque_item
+(uuid, title, price, description, created_on, type_item, estoque_id)
+VALUES(?, '', 0, '', '', '', 0);
+
--- api/estoque/models.py
+++ api/estoque/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from  datetime import datetime
 from django.utils import timezone
 import uuid
-# reate your models here.
 
+# reate your models here.
 class Item(models.Model):
     uuid=models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     title=models.CharField(max_length=500)
@@ -11,7 +11,7 @@ class Item(models.Model):
     description=models.TextField(max_length=500)
     created_on=models.DateTimeField(auto_now_add=True)
     type_item= models.CharField(max_length=255)
-    estoque=models.ForeignKey("Estoque", on_delete=models.DO_NOTHING, null=True)
+    estoque=models.ForeignKey("Estoque", on_delete=models.DO_NOTHING, null=True, blank=True)
 
     def __str__(self):
         return str(str(self.title) + ' ' + str(self.title) +  ' '+ str(self.price))
@@ -20,6 +20,20 @@ class Estoque(models.Model):
     uuid=models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     name=models.CharField(max_length=50, null=False, default='Meu estoque')  
     create_on=models.DateTimeField(default=timezone.now)
+    minimum=models.BigIntegerField(default=5)
+
+
+    def __str__(self):
+        return str(str(self.uuid) + ' ' + str(self.name) +  ' '+ str(self.minimum) + ' ' + str(self.create_on))
+    
+    '''get the itens in database'''
+    def get_items(self, estoque):
+         count=self.Item.objects.filter(estoque=estoque).count() 
+         return count
+         
+
+
+
 
 
 
